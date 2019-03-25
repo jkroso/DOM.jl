686156cb243ba8dc301e2bd63f45b1dd7b41d7e8
Jake Rosoman <jkroso@gmail.com>
2019-03-25 17:43:15 +1100

    basic support for latexstrings

---
+++ latex.jl
@@ -0,0 +1,24 @@
+@require "github.com/stevengj/LaTeXStrings.jl" @L_str LaTeXString
+@require "./html.jl"
+@require "." Node
+
+"""
+Enables you to render LaTeXStrings. Needs the text2svg command installed
+
+```julia
+@dom[:div L"\\mathrm{length}(\\textrm{city})"])
+```
+"""
+Base.convert(::Type{Node}, s::LaTeXString) =
+  open(cache(s), "r") do io
+    parse(MIME("text/html"), io)
+  end
+
+cache(str) = begin
+  s = String(str)[2:end-1]
+  p = joinpath(tempdir(), string(hash(s)))
+  if !ispath(p)
+    run(pipeline(`tex2svg $s`, p))
+  end
+  p
+end

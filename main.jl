8896a7807b518d189c0f58c1b6e82d8bee24c0aa
Jake Rosoman <jkroso@gmail.com>
2025-10-14 22:40:06 +1100

    avoid warning

--- main.jl
+++ main.jl
@@ -358,6 +358,8 @@ showstyles(io) =
 const reset = @dom[:style "html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}body{height:100%;width:100%;position:absolute}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select,textarea{margin:0}html{box-sizing:border-box}*{box-sizing:inherit}*:before,*:after{box-sizing:inherit}img,embed,object,audio,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0;text-align:left}"]
 
 "Defines an HTML document with the default stylesheets included"
+function HTML end
+
 HTML(attrs, children) =
   @dom[:html
     [:head [:meta charset="utf-8"] reset need(css[])]
